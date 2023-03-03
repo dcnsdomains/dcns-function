a9b8d71fd439723468864857f10c292be864515f
tomoki_sun <tomoki_69386@yahoo.co.jp>
2023-03-03 15:17:47 +0900

    Add contract abi

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * from './contracts'
 export * from './provider'
 export * from './registrar'
+
+import * as DcNSRegistryContract from './abis/DcNSRegistry.json'
+import * as PublicResolverContract from './abis/PublicResolver.json'
+import * as NamedRegistrarContract from './abis/NamedRegistrar.json'
+import * as DcRegistrarControllerContract from './abis/DcRegistrarController.json'
+import * as ReverseRegistrarContract from './abis/ReverseRegistrar.json'
+import * as ERC721DatastoreContract from './abis/ERC721Datastore.json'
